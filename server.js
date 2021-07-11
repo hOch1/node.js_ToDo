5eff1ef186f1fba6d656bd0c83bafc23d10f91c9
h0ch1 <h0ch1@naver.com>
2021-07-11 11:33:54 +0000

    .

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-// body-parser
-app.use(express.urlencoded({extended: true}));
+const dburl = "mongodb+srv://h0ch1:a02070203@nodetest.kijps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-app.listen(8080, () => {
-    console.log('server start');
+// app 설정
+app.use(express.urlencoded({extended: true}));
+app.set('view engine' , 'ejs');
+
+
+// DB & Server
+var db
+MongoClient.connect(dburl, (err, client) => {
+    if(err) return console.log(err);
+
+    db = client.db('todoapp');
+
+    app.listen(8080, () => {
+        console.log('server start');
+    });
 });
 
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html');
 });
@@ -18,6 +32,19 @@ app.get('/write', (req, res) => {
 
 app.post('/add', (req, res) => {
     res.send('전송완료');
-    console.log(req.body.title);
-    console.log(req.body.date);
+    db.collection('counter').findOne('')
+    db.collection('post').insertOne({제목 : req.body.title, 날짜 : req.body.date}, (err, result)=>{
+        if(err) return console.log(err);
+        console.log('저장완료');
+    });
+});
+
+
+
+app.get('/list', (req, res) => {
+    db.collection('post').find().toArray((err, result) =>{
+        if(err) return console.log(err);
+        console.log(result);
+        res.render('list.ejs', {posts : result});
+    });
 });
